f2c99f408e7dc473a25c6473548a4cf19f53f174
cuongtr <cuong.tran@gtoken.com>
2016-07-19 21:13:33 +0700

    Initial commit

---
+++ instagramapp/views/home.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    response = HttpResponse();
+    response.write("<h1>Welcome</h1>")
+    response.write("This is the polls app")
+    return response
